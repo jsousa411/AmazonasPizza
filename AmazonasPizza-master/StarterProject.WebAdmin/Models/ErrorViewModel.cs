53277c72b3d63f48c8697ec646851842a66ab50b
Flavio H Sousa <flavioh007@gmail.com>
2022-04-11 17:30:19 -0300

    Delete AmazonasPizza-master directory

--- AmazonasPizza-master/StarterProject.WebAdmin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StarterProject.WebAdmin.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
